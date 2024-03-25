23085d01f995179de85cabdd21718591245880fd
Abhishek Tiwari <92644513+abhiishek-00@users.noreply.github.com>
2024-03-25 23:19:11 +0530

    Create extract.py

---
+++ extract.py
@@ -0,0 +1,29 @@
+import os
+import csv
+import pandas as pd
+#from transform import transform_data
+
+data = []
+input_folder = './logs'
+output_file = 'collated.log'
+output_csv = 'collated.csv'
+fpOut = open(output_file, 'w')
+header = "date time s-ip cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) sc-status sc-substatus sc-win32-status time-taken"
+fpOut.write("{}\n".format(header))
+
+for file in os.listdir(input_folder):
+    if file.endswith(".log"):
+        print(file)
+        with open(os.path.join(input_folder, file), 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.rstrip()
+                if not("#Software" in line or "#Version" in line or "#Date" in line or "#Fields" in line):
+                    fpOut.write("{}\n".format(line))
+                    #print(line)
+                    #data.append(line)
+                    #print(data)
+
+df = pd.read_csv('collated.log', sep='\s', engine='python')
+df.to_csv('collated.csv', index=None)
+print(list(df.columns.values))
