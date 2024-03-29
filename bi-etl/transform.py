cba5c0c627dfe05496aa4070b46721f1c6e10ee9
Abhishek Tiwari <92644513+abhiishek-00@users.noreply.github.com>
2024-03-30 05:01:56 +0530

    Update transform.py

--- bi-etl/transform.py
+++ bi-etl/transform.py
@@ -3,7 +3,7 @@ import csv
 import pandas as pd
 import numpy as np
 import requests
-import urllib.request
+import urllib.request, urllib.error
 import json
 from collections import defaultdict
 
@@ -12,8 +12,8 @@ df = pd.read_csv('extracted.csv')
 print(list(df.columns.values))
 
 # transform date & time to year, quarter, month, week, day & hour
-print(df['date'].values[0])
-print(df['time'].values[0])
+# print(df['date'].values[0])
+# print(df['time'].values[0])
 df.date = pd.to_datetime(df.date, errors='coerce')
 df = df.dropna(subset=['date'])
 df.time = pd.to_datetime(df.time, format="%H:%M:%S", errors='coerce')
@@ -24,21 +24,31 @@ df['month'] = df.date.apply(lambda x: x.strftime('%B-%Y'))
 df['week'] = df['date'].dt.to_period('W-SAT').apply(lambda r: r.start_time).dt.date
 df['day'] = df.date.dt.day_name()
 df['hour'] = df.time.dt.hour
-print(df['year'].values[0])
-print(df['quarter'].values[0])
-print(df['month'].values[0])
-print(df['week'].values[0])
-print(df['day'].values[0])
-print(df['hour'].values[0])
+# print(df['year'].values[0])
+# print(df['quarter'].values[0])
+# print(df['month'].values[0])
+# print(df['week'].values[0])
+# print(df['day'].values[0])
+# print(df['hour'].values[0])
 
 # get location from an IP using geolocation-db.com
 def getLocation(ip):
-  with urllib.request.urlopen("https://geolocation-db.com/jsonp/"+ip) as url:
-    data = url.read().decode()
-    data = data.split("(")[1].strip(")")
-    return [json.loads(data)['country_name'], json.loads(data)['city']]
+    url = "https://geolocation-db.com/jsonp/"+ip
+    try:
+        res = urllib.request.urlopen(url)
+    except urllib.error.HTTPError as e:
+        # Return code error (e.g. 404, 501, ...)
+        print('HTTPError: {}'.format(e.code))
+    except urllib.error.URLError as e:
+        # Not an HTTP-specific error (e.g. connection refused)
+        print('URLError: {}'.format(e.reason))
+    else:
+        # 200
+        data = res.read().decode()
+        data = data.split("(")[1].strip(")")
+        return [json.loads(data)['country_name'], json.loads(data)['city']]
 
-print(df['c-ip'].values[11])
+# print(df['c-ip'].values[11])
 ips = []
 countries = []
 cities = []
@@ -48,21 +58,40 @@ ips1 = []
 filetypes = []
 ostypes = []
 browsertypes = []
+l = 1
+# drop rows with empty values in column c-ip
+df['c-ip'].str.strip().replace('', np.nan, inplace=True)
+df.dropna(subset=['c-ip'], inplace=True)
 for index, row in df.iterrows():
+   print("processing row number "+str(l))
+   l = l + 1
    # transform IP into country & city 
    data = row['c-ip']
    ips.append(data)
    if data not in ips1:
     ips1.append(data)
+    print("calling geolocation API for " + row['c-ip'])
     response = getLocation(row['c-ip'])
     if response != None:
-            countries.append(response[0])
-            cities.append(response[1])
-            dcountry[data] = response[0]
-            dcity[data] = response[1]
+            tcountry = ''
+            tcity = ''
+            if str(response[0]) == "None":
+                tcountry = 'NA'
+            else:
+                tcountry = response[0]
+            if str(response[1]) == "None":
+                tcity = 'NA'
+            else:
+                tcity = response[1]
+            countries.append(tcountry)
+            cities.append(tcity)
+            dcountry[data] = tcountry
+            dcity[data] = tcity
     else:
-            countries.append(np.nan)
-            cities.append(np.nan)
+            countries.append('NA')
+            cities.append('NA')
+            dcountry[data] = 'NA'
+            dcity[data] = 'NA'
    else:
        countries.append(dcountry[data])
        cities.append(dcity[data])
@@ -113,9 +142,9 @@ df_ips = pd.DataFrame(locations, columns = ['ip', 'country', 'city'])
 df['ip'] = df_ips['ip']
 df['country'] = df_ips['country']
 df['city'] = df_ips['city']
-print(df['ip'].values[11])
-print(df['country'].values[11])
-print(df['city'].values[11])   
+# print(df['ip'].values[11])
+# print(df['country'].values[11])
+# print(df['city'].values[11])   
 
 files = {'filetype' :filetypes, 'os' :ostypes, 'browser' :browsertypes}
 df_requests = pd.DataFrame(files, columns = ['filetype', 'os', 'browser'])
@@ -123,21 +152,21 @@ df['uri'] = df['cs-uri-stem']
 df['filetype'] = df_requests['filetype']
 df['os'] = df_requests['os']
 df['browser'] = df_requests['browser']
-print(df['cs-uri-stem'].values[12])
-print(df['uri'].values[12])
-print(df['filetype'].values[12])
-print(df['cs(User-Agent)'].values[12])
-print(df['os'].values[12])
-print(df['browser'].values[12])
+# print(df['cs-uri-stem'].values[12])
+# print(df['uri'].values[12])
+# print(df['filetype'].values[12])
+# print(df['cs(User-Agent)'].values[12])
+# print(df['os'].values[12])
+# print(df['browser'].values[12])
 
 df['statuscode'] = df['sc-status']
 df['timetaken'] = df['time-taken']
-print(df['statuscode'].values[12])
-print(df['timetaken'].values[12])
-print(list(df.columns.values))
+# print(df['statuscode'].values[12])
+# print(df['timetaken'].values[12])
+# print(list(df.columns.values))
 
 # drop old columns from the dataframe
 df = df.drop(['date', 'time', 's-ip', 'cs-method', 'cs-uri-stem', 'cs-uri-query', 's-port', 'cs-username', 'c-ip', 'cs(User-Agent)', 'sc-status', 'sc-substatus', 'sc-win32-status', 'time-taken'], axis=1)
 print(list(df.columns.values))
 df.to_csv('transformed.csv', index=None)
-print(list(df.columns.values))
+# print(list(df.columns.values))
