0c60f5a4290c9781f00935490c605c999926ea3d
Abhishek Tiwari <92644513+abhiishek-00@users.noreply.github.com>
2024-04-02 02:16:27 +0530

    Update elt-pipeline.py

--- bi-etl/apache-airflow/dags/elt-pipeline.py
+++ bi-etl/apache-airflow/dags/elt-pipeline.py
@@ -7,6 +7,8 @@ import subprocess
 from extract import extractData
 from transform import transformData
 from load import loadData
+from pendulum import datetime, duration
+from transform import checkIfTransformCSVPresent
 
 # install dependencies
 def installDependencies():
@@ -14,6 +16,7 @@ def installDependencies():
     subprocess.run(['pip','install','psycopg2'])
     subprocess.run(['pip','install','pandas'])
     subprocess.run(['pip','install','Requests'])
+    subprocess.run(['pip','install','pendulum'])
 
 def startPipeline():
     print("start the pipeline")
@@ -23,8 +26,16 @@ def endPipeline():
 
 # define the DAG
 logs_etl_dag = DAG(dag_id='logs-etl',
-                         description='An ETL pipeline analysing internet log files',
-                         start_date=datetime(2024,4,1))
+                         description='An ETL pipeline for analysing internet log files',
+                         max_active_runs=1,
+                         start_date=datetime(2024,4,1),
+                         catchup=False,
+                             default_args={
+                                "retries": 3,
+                                "retry_delay": duration(seconds=60),
+                                "retry_exponential_backoff": True,
+                                "max_retry_delay": duration(hours=2),
+    })
 
 # define the tasks
 start_pipeline = PythonOperator(task_id='start_pipeline',
@@ -37,7 +48,7 @@ extract_data = PythonOperator(task_id='extract_data',
                      python_callable=extractData,
                      dag=logs_etl_dag)
 transform_data = PythonOperator(task_id='transform_data',
-                       python_callable=transformData,
+                       python_callable=checkIfTransformCSVPresent,
                        dag=logs_etl_dag)
 load_data = PythonOperator(task_id='load_data',
                        python_callable=loadData,
