6c609ee9337b998f0112bf82f661acf9051912c0
Abhishek Tiwari <92644513+abhiishek-00@users.noreply.github.com>
2024-04-02 16:32:34 +0530

    Create load-windows.py

---
+++ bi-etl/load-windows.py
@@ -0,0 +1,114 @@
+import psycopg2
+
+def loadTables(tablename, createQuery, csv_file_path, cur):
+    # drop table if exists
+    cur.execute('DROP TABLE IF EXISTS '+ tablename + ' cascade')
+    cur.execute(createQuery)
+    # copy data from csv to the table
+    copyQuery = f"""
+        COPY {tablename} FROM stdin 
+        DELIMITER as ','
+        CSV HEADER;
+    """
+    with open(csv_file_path, 'r',encoding='ISO-8859-1') as f:
+        cur.copy_expert(sql=copyQuery, file=f)
+
+dbvalues = {
+    "dbname":"reema",
+    "user":"postgres",
+    "password":""
+}
+
+# connect to DB
+connectionString = "host=localhost dbname=" + dbvalues['dbname'] + " user=" + dbvalues['user'] + " password=" + dbvalues['password']
+conn = psycopg2.connect(connectionString)
+cur = conn.cursor()
+# set automatic commit to be true, so that each action is committed without having to call conn.commit() after each command
+conn.set_session(autocommit=True)
+
+# create table projectetl
+tablename = 'projectetl'
+csv_file_path = 'transformed.csv'
+createQuery = 'CREATE TABLE '+ tablename + "("
+createQuery += """
+        id int,
+        year int,
+        quarter text,
+        month text,
+        week date,
+        day text,
+        hour int,
+        ip text,
+        country text,
+        city text,
+        uri text,
+        filetype text,
+        os text,
+        browser text,
+        statuscode int,
+        timetaken int,
+        PRIMARY KEY (id)
+    )
+    """
+loadTables(tablename, createQuery, csv_file_path, cur)
+
+# create relational tables
+tablename = 'etldatetable'
+csv_file_path = 'etldatetable.csv'
+createQuery = 'CREATE TABLE '+ tablename + "("
+createQuery += """
+        id text,
+        year int,
+        quarter text,
+        month text,
+        week date,
+        day text,
+        hour int,
+        PRIMARY KEY (id)
+    )
+    """
+loadTables(tablename, createQuery, csv_file_path, cur)
+tablename = 'eltclienttable'
+csv_file_path = 'eltclienttable.csv'
+createQuery = 'CREATE TABLE '+ tablename + "("
+createQuery += """
+        id text,
+        ip text,
+        country text,
+        city text,
+        PRIMARY KEY (id)
+    )
+    """
+loadTables(tablename, createQuery, csv_file_path, cur)
+tablename = 'etlrequesttable'
+csv_file_path = 'etlrequesttable.csv'
+createQuery = 'CREATE TABLE '+ tablename + "("
+createQuery += """
+        id text,
+        uri text,
+        filetype text,
+        os text,
+        browser text,
+        PRIMARY KEY (id)
+    )
+    """
+loadTables(tablename, createQuery, csv_file_path, cur)
+tablename = 'etlmaintable'
+csv_file_path = 'etlmaintable.csv'
+createQuery = 'CREATE TABLE '+ tablename + "("
+createQuery += """
+        id int,
+        statuscode int,
+        timetaken int,
+        dateid text references etldatetable(id),
+        clientid text references eltclienttable(id),
+        requestid text references etlrequesttable(id),
+        PRIMARY KEY (id)
+    )
+    """
+loadTables(tablename, createQuery, csv_file_path, cur)
+
+# commit the changes and close the connection to the default database
+conn.commit()
+cur.close()
+conn.close()
