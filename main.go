8ad56b89b460cbb9a944ae63d531a02179eaab0a
Abhishek Tiwari <92644513+abhiishek-00@users.noreply.github.com>
2024-08-20 12:11:11 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,374 +1,74 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"strings"
+	"os"
+
+	"gopkg.in/yaml.v2"
+	"github.com/google/uuid"
 )
 
-type InputNode struct {
-	ApplicationName string       `json:"application_name"`
-	CarID           string       `json:"car_id"`
-	NodeDetails     []NodeDetail `json:"node_details"`
-}
-
 type NodeDetail struct {
-	NodeName   string `json:"node_name"`
-	NodeID     string `json:"node_id"`
-	ParentID   string `json:"parent_id"`
-	IsCritical bool   `json:"is_critical"`
-	IPC1       IPC    `json:"ipc1"`
-	IPC2       IPC    `json:"ipc2"`
-	IPC2R2     IPC    `json:"ipc2r2"`
+	NodeName   string                 `yaml:"node_name"`
+	DependsOn  string                 `yaml:"depends_on"`
+	IsCritical bool                   `yaml:"is_critical"`
+	IPC1       map[string]interface{} `yaml:"ipc1"`
+	IPC2       map[string]interface{} `yaml:"ipc2"`
+	IPC2R2     map[string]interface{} `yaml:"ipc2r2"`
+	NodeID     string                 `yaml:"node_id"`
+	ParentID   string                 `yaml:"parent_id"`
 }
 
-type IPC struct {
-	F5   string `json:"f5"`
-	GTM  string `json:"gtm"`
-	IP   string `json:"ip"`
-	Port int    `json:"port"`
-}
-
-type OutputNode struct {
-	ID       string       `json:"id"`
-	Name     string       `json:"name"`
-	State    NodeState    `json:"state"`
-	Children []OutputNode `json:"children,omitempty"`
-}
-
-type NodeState struct {
-	IPC1   bool   `json:"ipc1"`
-	IPC2   bool   `json:"ipc2"`
-	IPC2R2 string `json:"ipc2r2"`
-}
-
-type Output struct {
-	Status  int          `json:"status"`
-	Message string       `json:"message"`
-	Data    []OutputNode `json:"data"`
+type InputYAML struct {
+	ApplicationName string      `yaml:"application_name"`
+	NodeDetails     []NodeDetail `yaml:"node_details"`
 }
 
 func main() {
-	inputJSON := `{
-		"application_name": "Enterprise Login",
-		"car_id": "123456789",
-		"node_details": [
-			{
-				"node_name": "Web",
-				"node_id": "web_123",
-				"parent_id": "123456789",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.8.141.125",
-					"port": 443
-				},
-				"ipc2": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "MFA POD",
-				"node_id": "mfapod_123",
-				"parent_id": "web_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "CBIS-23",
-				"node_id": "cbis23_123",
-				"parent_id": "mfapod_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Mobile",
-				"node_id": "mobile_123",
-				"parent_id": "123456789",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "MFA POA",
-				"node_id": "mfapoa_123",
-				"parent_id": "mobile_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Redis",
-				"node_id": "redis_123",
-				"parent_id": "mfapoa_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "MFA PPP",
-				"node_id": "mfappp_123",
-				"parent_id": "mobile_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Tokenizer",
-				"node_id": "tokenizer_123",
-				"parent_id": "mfappp_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Redis",
-				"node_id": "redis_123",
-				"parent_id": "tokenizer_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "MFA POD",
-				"node_id": "mfapod_123",
-				"parent_id": "mobile_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "CBIS",
-				"node_id": "cbis_123",
-				"parent_id": "123456789",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Verf",
-				"node_id": "verf_123",
-				"parent_id": "cbis_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Risk Dispatcher",
-				"node_id": "risk_123",
-				"parent_id": "cbis_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Push AuthN",
-				"node_id": "push_123",
-				"parent_id": "cbis_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Agent Binding",
-				"node_id": "agentb123_123",
-				"parent_id": "push_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "DSS",
-				"node_id": "dss_123",
-				"parent_id": "push_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "MFA DB",
-				"node_id": "mfadb_123",
-				"parent_id": "cbis_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			},
-			{
-				"node_name": "Risk Dispatcher",
-				"node_id": "risk_123",
-				"parent_id": "mfadb_123",
-				"is_critical": true,
-				"ipc1": {
-					"f5": "phx-dc1ecpgtm1a.phx.aexp.com",
-					"gtm": "enterpriseidentitysreportal.igdha-ecp.aexp.com",
-					"ip": "10.34.131.77",
-					"port": 443
-				},
-				"ipc2": {},
-				"ipc2r2": {}
-			}
-		]
-	}`
-
-	var input InputNode
-	err := json.Unmarshal([]byte(inputJSON), &input)
+	// Read the input YAML file
+	inputFile, err := os.ReadFile("/mnt/data/input.yaml")
 	if err != nil {
-		log.Fatalf("Error unmarshaling input JSON: %v", err)
+		log.Fatalf("Failed to read input file: %v", err)
 	}
 
-	output := Output{
-		Status:  200,
-		Message: "Success",
-		Data: []OutputNode{
-			{
-				ID:    input.CarID,
-				Name:  input.ApplicationName,
-				State: NodeState{},
-			},
-		},
-	}
-
-	nodeMap := make(map[string]*OutputNode)
-	nodeMap[input.CarID] = &output.Data[0] // Adding a placeholder for the root node
-
-	for _, inputNode := range input.NodeDetails {
-		processNode(&inputNode, nodeMap)
-	}
-
-	outputJSON, err := json.MarshalIndent(output, "", "    ")
+	// Unmarshal the input YAML
+	var inputData InputYAML
+	err = yaml.Unmarshal(inputFile, &inputData)
 	if err != nil {
-		log.Fatalf("Error marshaling output JSON: %v", err)
+		log.Fatalf("Failed to unmarshal input YAML: %v", err)
 	}
 
-	fmt.Println(string(outputJSON))
-}
+	// Create a map to store node_name to node_id mapping
+	nodeIDMap := make(map[string]string)
 
-func processNode(inputNode *NodeDetail, nodeMap map[string]*OutputNode) {
-	parentID := strings.TrimSpace(inputNode.ParentID)
+	// Iterate over each node and generate UUIDs for node_id and parent_id
+	for i, node := range inputData.NodeDetails {
+		// Generate node_id UUID
+		nodeUUID := uuid.New().String()
+		inputData.NodeDetails[i].NodeID = nodeUUID
+		nodeIDMap[node.NodeName] = nodeUUID
 
-	state := NodeState{
-		IPC1:   inputNode.IPC1.IP != "",
-		IPC2:   inputNode.IPC2.IP != "",
-		IPC2R2: "NA",
+		// Generate parent_id UUID based on depends_on field
+		if parentID, exists := nodeIDMap[node.DependsOn]; exists {
+			inputData.NodeDetails[i].ParentID = parentID
+		} else {
+			inputData.NodeDetails[i].ParentID = "" // No parent, or parent not yet processed
+		}
 	}
 
-	outputNode := OutputNode{
-		ID:    inputNode.NodeID,
-		Name:  inputNode.NodeName,
-		State: state,
+	// Marshal the modified data back into YAML
+	outputYAML, err := yaml.Marshal(&inputData)
+	if err != nil {
+		log.Fatalf("Failed to marshal output YAML: %v", err)
 	}
 
-	parentNode, exists := nodeMap[parentID]
-	if exists {
-		parentNode.Children = append(parentNode.Children, outputNode)
-		nodeMap[outputNode.ID] = &parentNode.Children[len(parentNode.Children)-1]    // Update nodeMap with the new child node reference
-		nodeMap[inputNode.NodeID] = &parentNode.Children[len(parentNode.Children)-1] // Update nodeMap with the new child node ID
-	} else {
-		fmt.Printf("Parent node with ID %s not found\n", parentID)
-		return
+	// Write the output YAML to a file
+	err = ioutil.WriteFile("/mnt/data/output.yaml", outputYAML, 0644)
+	if err != nil {
+		log.Fatalf("Failed to write output file: %v", err)
 	}
+
+	fmt.Println("YAML transformation complete. Output written to /mnt/data/output.yaml")
 }
